44ad5cb57df610329285a2c0ab3c54b3468bb637
Daniel Acquesta <danielacquesta@sbcglobal.net>
2017-06-16 14:26:40 -0700

    added check item and render functions

--- app.js
+++ app.js
@@ -6,26 +6,36 @@ const shoppingState = {
 
 //2 functions that modify state
 // add item to list
-function addItem(state, item) {
+function addItem(state, itemName) {
 	state.list.push({
-		displayName:item,
+		displayName: itemName,
 		checkedOff: false 
 	});
 }
 
-function removeItem(state, targetItem) {
-	let deletedItem = state.list.find(function(stateItem) {
-		return stateItem.displayName === targetItem;	
-	});
-	let index = state.list.indexOf(deletedItem);
+// function getItem(state, index)
+
+function checkItem(state, index) {
+	// target correct item in array
+	// change its checked property?
+
+	state.list[index].checkedOff = !state.list[index].checkedOff;
+}
+
+function removeItem(state, index) {
+
+	// let deletedItem = state.list.find(function(stateItem) {
+	// 	return stateItem.displayName === targetItem;	
+	// });
+	// let index = state.list.indexOf(deletedItem);
 	state.list.splice(index, 1);
 }
 
 
-addItem(shoppingState, 'yogurt');
-console.log(shoppingState);
-removeItem(shoppingState, 'yogurt');
-console.log(shoppingState);
+//addItem(shoppingState, 'yogurt');
+//console.log(shoppingState);
+//removeItem(shoppingState, 'yogurt');
+//console.log(shoppingState);
 
 
 
@@ -36,13 +46,13 @@ console.log(shoppingState);
 // remove item from list
 
 //let template = " all the html we need to add"
-//3 functions that render state
+//3, functions that render state
 
 function renderItem(state, element) {
 	let itemsHtml = state.list.map(function(item) {
 		return (`
 			<li>
-        <span class="shopping-item">${item}</span>
+        <span class="shopping-item">${item.displayName}</span>
         <div class="shopping-item-controls">
           <button class="shopping-item-toggle">
             <span class="button-label">check</span>
@@ -65,13 +75,13 @@ function renderItem(state, element) {
 //4 event listeners
 $(function() {
 	$('#js-shopping-list-form').on('submit', function(event){
+		event.preventDefault();
 		addItem(shoppingState, $('#shopping-list-entry').val());
-		console.log(shoppingState);
-
-		//renderItem(shoppingState, 'ul');
+		renderItem(shoppingState, 'ul');
 
 	})
 });
+console.log(shoppingState);
 // when user clicks/return call the add function
 //all the functions from step 2
 //${function(){
