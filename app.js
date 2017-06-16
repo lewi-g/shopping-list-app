99d9ab8e386613b88b447bce1674aeda1af8c625
lewi-g <dr.lewi@gmail.com>
2017-06-16 12:20:53 -0400

    first step

---
+++ app.js
@@ -0,0 +1,37 @@
+// 1 global state
+const shoppingState = {
+	list : [],
+};
+
+//2 functions that modify state
+// add item to list
+function addItem(state, item) {
+	state.list.push(item);
+}
+// check and uncheck item on list listening to "check" buton
+function checkButton(){
+	if 
+
+
+	$('button' '.shopping-item-toggle')
+}
+
+//function checkButton(item) {
+	if apples=check {
+		uncheck;
+	}
+	if apples=unceck {
+		check
+	}
+}
+// remove item from list
+
+
+//3 functions that render state
+
+//let template = " all the html we need to add"
+
+//4 event listeners
+
+// when user clicks/return call the add function
+//all the functions from step 2
